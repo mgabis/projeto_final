4b87f558fcb30696b556dfe552d410e395cf188d
mgabis <gabis.97.mgs@gmail.com>
2015-06-18 20:23:09 -0300

    18/06

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
@@ -13,58 +14,105 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
 
-<?php $this->beginBody() ?>
-    <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => 'My Company',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
-                    ['label' => 'Pessoas', 'url' => ['/pessoas/todas']],
-                    Yii::$app->user->isGuest ?
-                        ['label' => 'Login', 'url' => ['/site/login']] :
-                        ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                            'url' => ['/site/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
-                ],
-            ]);
-            NavBar::end();
-        ?>
+        <?php $this->beginBody() ?>
+        <div class="wrap">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#/site/index"></a>
+                    </div>
 
-        <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= $content ?>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#site/index">início <span class="sr-only">(current)</span></a></li>
+                            <li><a href="#site/about">Sobre</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Eventos<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Cinema</a></li>
+                                    <li><a href="#">Teatro</a></li>
+                                    <li><a href="#">Baladas</a></li>
+                                    <li><a href="#">Outros</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                        <li><a href="#/site/contact">Contato</a></li>
+                        <li><a href="#/site/todas">Pessoas</a></li>
+                        </ul>
+                        <form class="navbar-form navbar-left" role="search">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                        <ul class="nav navbar-nav navbar-right">
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+            <?php
+            /* NavBar::begin([
+              'brandLabel' => 'My Ticket.com',
+              'brandUrl' => Yii::$app->homeUrl,
+              'options' => [
+              'class' => 'navbar-inverse navbar-fixed-top',
+              ],
+              ]);
+              echo Nav::widget([
+              'options' => ['class' => 'navbar-nav navbar-right'],
+              'items' => [
+              ['label' => 'Início', 'url' => ['/site/index']],
+              ['label' => 'Sobre', 'url' => ['/site/about']],
+              ['label' => 'Contato', 'url' => ['/site/contact']],
+              ['label' => 'Pessoas', 'url' => ['/site/todas']],
+              Yii::$app->user->isGuest ?
+              ['label' => 'Login', 'url' => ['/site/login']] :
+              ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+              'url' => ['/site/logout'],
+              'linkOptions' => ['data-method' => 'post']],
+              ],
+              ]);
+              NavBar::end(); */
+            ?>
+
+            <div class="container">
+                <?=
+                Breadcrumbs::widget([
+                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                ])
+                ?>
+                <?= $content ?>
+            </div>
         </div>
-    </div>
 
-    <footer class="footer">
-        <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-            <p class="pull-right"><?= Yii::powered() ?></p>
-        </div>
-    </footer>
+        <footer class="footer">
+            <div class="container">
+                <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+                <p class="pull-right"><?= Yii::powered() ?></p>
+            </div>
+        </footer>
 
-<?php $this->endBody() ?>
-</body>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
--- views/site/about.php
+++ views/site/about.php
@@ -1,16 +1,19 @@
 <?php
+
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
+$this->title = 'Sobre a empresa';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
 
+    <h4> A My Tickets Produtora, está há dois anos no mercado de trabalho, 
+        realiza eventos de todos os portes, procurando sempre proporcionar o bem estar
+        de seus clientes em seus eventos. Trabalhamos com shows nacionais, internacionais
+        e eventos de grande porte. Além de vendermos os ingressos dos respectivos shows, eventos
+        e marketing relacionado ao evento.</h4>
     <code><?= __FILE__ ?></code>
 </div>
