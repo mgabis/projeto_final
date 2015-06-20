37cffcb6c1cefe542c8d3c7cd93f395e6a93d292
mgabis <gabis.97.mgs@gmail.com>
2015-06-19 21:47:07 -0300

    19/06

--- views/site/index.php
+++ views/site/index.php
@@ -1,51 +1,66 @@
 <?php
-use yii\helpers\BaseUrl as Url; 
+
+use yii\helpers\BaseUrl as Url;
+use yii\bootstrap\Carousel;
+
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-       
-        <img src="<?= Url::to(['img/logo.png']);?>" alt="..." class="img-rounded">
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <div class="row">
 
-       
+            <?php
+            echo Carousel::widget([
+                'items' => [
+                    // the item contains only the image
+                    '<img style="margin-left:auto; margin-right:auto" src="' . Url::to(['img/sonata_arctica.jpg']) . '"/>',
+                    '<img style="margin-left:auto; margin-right:auto" src="' . Url::to(['img/reacap.jpg']) . '"/>',
+                    '<img style="margin-left:auto; margin-right:auto" src="' . Url::to(['img/Sonata.png']) . '"/>',
+                ]
+            ]);
+            ?>
+
+
+        </div>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2><img src="<?= Url::to(['img/sonata_arctica.jpg']);?>" alt="..." class="img-rounded"></h2>
+                <img src="<?= Url::to(['img/images.jpg']); ?>" alt="..." class="img-rounded">
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <h2>Sonata Arctica</h2>
+                <p>Ainda que originária da gélida Finlândia, a banda Sonata Arctica desembarcará em Porto Alegre
+                    para aquecer ainda mais os corações metálicos da gauchada. Trabalhando na divulgação
+                    de seu mais recente disco, Pariah’s Child – sem deixar de fora do repertório belas composições
+                    dos álbuns anteriores –, o quinteto de power metal europeu se apresentará dia 27 de julho de 2015,
+                    às 20h, no Opinião (Rua José do Patrocínio, 834).</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <img src="<?= Url::to(['img/reacao_cadeia.jpg']); ?>" alt="..." class="img-rounded">
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <h2>Reação em cadeia</h2>
+                <p>A Reação em Cadeia está completando 15 anos em 2015! Para comemorar, a banda realizará nos dias 29 e 30 de julho
+                    um show acústico com os seus maiores sucessos para aquuecer os fãs neste inverno. O show será no Teatro CIEE
+                    às 21 horas, R. Dom Pedro II, 861 - Higienópolis, Porto Alegre - RS.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <img src="<?= Url::to(['img/neon.jpg']); ?>" alt="..." class="img-rounded">
+                <h2>Neon</h2>
+                <p>Os DJs residentes Cevallos e Kahara apresentam mais uma NEON com o foco nos talentos locais!
+                    O convidado da vez é um dos residentes da festa Wax, Henrique Casanova aka Apoena. Apoena é DJ/produtor gaúcho
+                    com reconhecimento internacional, um dos poucos no país inserido na indústria internacional do vinil, com discos
+                    de Techno e Deep House lançados por diversos selo pelo mundo, inclusive o seu próprio Allnite Music.</p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
         </div>
 
